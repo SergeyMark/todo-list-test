935b4ffd57e8706684897af7b91936641f3fb254
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-01-24 14:33:07 +0200

    add

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btnOnToDoWork = document.querySelector('.button');
 const containerTextTodo = document.querySelector('.container-todo-text');
 
 let arr = [];
-let removeButton;
+// let removeButton;
 
 
 btnOnToDoWork.addEventListener('click', onBtnToDo);
@@ -14,8 +14,12 @@ function onBtnToDo(){
 
     containerTextTodo.innerHTML = createdMarkUp(arr);  
 
-    removeButton = document.querySelector('.todo-btn');
+    let removeButton = document.querySelector('.todo-btn');
     removeButton.addEventListener('click', onBtnDelete);
+
+    if (removeButton) {
+        arr.splice(0, 1);
+    }
 }
 
 
@@ -23,15 +27,15 @@ function createdMarkUp(array) {
     return array.map(item => 
         `<div class="box-todo-text">
             <p class="todo-text">${item}</p>
-            <button type="button" class="todo-btn">Delete</button>
+            <button type="button" class="todo-btn">Del</button>
         </div>`).join('');
 }
+
 
 function onBtnDelete() {
     const itemDelete = document.querySelector('.box-todo-text');
     itemDelete.remove();
 }
-
 
 
 
